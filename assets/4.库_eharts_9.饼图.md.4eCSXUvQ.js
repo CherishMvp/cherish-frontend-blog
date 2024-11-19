b4bdd9e2c38f882eb0bb35346aedba89397ee4c9
CherishMvp <CherishMvp@users.noreply.github.com>
2024-11-19 06:34:01 +0000

    deploy: 2b58f952b4929576648f751302f44f6cd257b47a

---
+++ assets/4.库_eharts_9.饼图.md.4eCSXUvQ.js
@@ -0,0 +1,22 @@
+import{_ as n,c as a,a2 as p,o as e}from"./chunks/framework.BeuUUHFt.js";const b=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"4.库/eharts/9.饼图.md","filePath":"4.库/eharts/9.饼图.md","lastUpdated":1731997977000}'),t={name:"4.库/eharts/9.饼图.md"};function l(o,s,u,r,i,q){return e(),a("div",null,s[0]||(s[0]=[p(`<div class="language- vp-adaptive-theme line-numbers-mode"><button title="Copy Code" class="copy"></button><span class="lang"></span><pre class="shiki shiki-themes min-dark one-dark-pro vp-code" tabindex="0"><code><span class="line"><span>option = {</span></span>
+<span class="line"><span>        title: {</span></span>
+<span class="line"><span>          text: &quot;告警次数统计图&quot;</span></span>
+<span class="line"><span>        },</span></span>
+<span class="line"><span>        color: [&quot;#3BA1FF&quot;],</span></span>
+<span class="line"><span>        xAxis: {</span></span>
+<span class="line"><span>          type: &quot;category&quot;,</span></span>
+<span class="line"><span>          data: [&quot;12.01&quot;, &quot;12.02&quot;, &quot;12.03&quot;, &quot;12.04&quot;, &quot;12.05&quot;, &quot;12.06&quot;, &quot;12.07&quot;, &quot;12.08&quot;, &quot;12.09&quot;, &quot;12.11&quot;, &quot;12.12&quot;, &quot;12.13&quot;, &quot;12.14&quot;, &quot;12.15&quot;, &quot;12.166&quot;, &quot;12.17&quot;, &quot;12.18&quot;, &quot;12.19&quot;, &quot;12.20&quot;, &quot;12.21&quot;, &quot;12.22&quot;, &quot;12.23&quot;, &quot;12.24&quot;, &quot;12.25&quot;, &quot;12.26&quot;, &quot;12.27&quot;, &quot;12.28&quot;, &quot;12.29&quot;, &quot;12.30&quot;, &quot;12.31&quot;]</span></span>
+<span class="line"><span>        },</span></span>
+<span class="line"><span>        yAxis: {</span></span>
+<span class="line"><span>          type: &quot;value&quot;</span></span>
+<span class="line"><span>        },</span></span>
+<span class="line"><span>        series: [</span></span>
+<span class="line"><span>          {</span></span>
+<span class="line"><span>            data: [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 3, 3, 2, 1],</span></span>
+<span class="line"><span>            type: &quot;bar&quot;,</span></span>
+<span class="line"><span>            backgroundStyle: {</span></span>
+<span class="line"><span>              color: &quot;rgba(180, 180, 180, 0.2)&quot;</span></span>
+<span class="line"><span>            }</span></span>
+<span class="line"><span>          }</span></span>
+<span class="line"><span>        ]</span></span>
+<span class="line"><span>      }</span></span></code></pre><div class="line-numbers-wrapper" aria-hidden="true"><span class="line-number">1</span><br><span class="line-number">2</span><br><span class="line-number">3</span><br><span class="line-number">4</span><br><span class="line-number">5</span><br><span class="line-number">6</span><br><span class="line-number">7</span><br><span class="line-number">8</span><br><span class="line-number">9</span><br><span class="line-number">10</span><br><span class="line-number">11</span><br><span class="line-number">12</span><br><span class="line-number">13</span><br><span class="line-number">14</span><br><span class="line-number">15</span><br><span class="line-number">16</span><br><span class="line-number">17</span><br><span class="line-number">18</span><br><span class="line-number">19</span><br><span class="line-number">20</span><br><span class="line-number">21</span><br><span class="line-number">22</span><br></div></div>`,1)]))}const m=n(t,[["render",l]]);export{b as __pageData,m as default};
